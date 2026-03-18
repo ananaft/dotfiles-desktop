7a5e538f29381a37c9c4d85f222d98bef49abcf5
ananaft <lukas.hoettges@gmx.de>
2026-03-18 17:48:45 +0100

    Waybar: added power utility

---
+++ .config/waybar/scripts/powermenu.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+choice=$(printf "Shutdown\nReboot\nReset" | \
+	wofi --dmenu --hide-search --lines 3 --width 180 --cache-file=/dev/null)
+
+case "$choice" in
+	Shutdown)
+		hyprshutdown -t 'Shutting down...' --post-cmd 'systemctl poweroff'
+		;;
+	Reboot)
+		hyprshutdown -t 'Restarting...' --post-cmd 'systemctl reboot'
+		;;
+	Reset)
+		hyprshutdown -t 'Resetting...' --post-cmd 'systemctl soft-reboot'
+		;;
+esac
